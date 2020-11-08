ce780e597c5bad7ab1b053a4ee090d58f71c4ff4
cneidson <carrie.eidson@nyu.edu>
2020-11-08 17:35:10 -0500

    Floods

--- js/scripts.js
+++ js/scripts.js
@@ -678,3 +678,30 @@ function isElementOnScreen(id) {
   var bounds = element.getBoundingClientRect();
   return bounds.top < window.innerHeight && bounds.bottom > 0;
 }
+
+//floods slider
+/**
+ * @param  {number} interval (in milliseconds)
+ */
+function cycleBackgrounds(interval) {
+  let index = 0
+
+  const $imageEls = document.querySelectorAll('.container .slide') // Get the images to be cycled.
+
+  setInterval(() => {
+    // Get the next index.  If at end, restart to the beginning.
+    index = index + 1 < $imageEls.length ? index + 1 : 0
+
+    // Show the next
+    $imageEls[index].classList.add('show')
+
+    // Find the previous.
+    const previous = index - 1 < 0 ? $imageEls.length - 1 : index - 1;
+
+    // Hide the previous
+    $imageEls[previous].classList.remove('show')
+  }, interval)
+}
+document.addEventListener("DOMContentLoaded", function() {
+  cycleBackgrounds(2000);
+});
